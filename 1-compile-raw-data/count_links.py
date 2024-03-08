a584f9d2f5d8bc27d42a2b90e6ff9aeb7a84783f
carolinejjung <cjung8321@gmail.com>
2024-03-07 23:29:18 -0500

    individual json

--- 1-compile-raw-data/count_links.py
+++
@@ -1,7 +0,0 @@
-import json
-
-path = "1-compile-raw-data/compiled_data.json"
-with open(path, "r") as file:
-    data = json.load(file)
-
-print(len(data))
--- 1-compile-raw-data/data_manipulation.py
+++ 1-compile-raw-data/data_manipulation.py
@@ -2,32 +2,33 @@
 import json
 import os
 
-def get_all_links():
-  dir_path = '/users/carolinejung/cs315-private/project2-files' # hardcoded for security purposes
-  files = os.listdir(dir_path)
-
+def get_all_links(file):
   all_links = []
-  for file in files:
-    print(file)
-    file_name = dir_path + "/" + file
-    with open(file_name, 'r') as myFile:
-      data = json.load(myFile)
-    myFile.close()
-
-    feed_dict = data["Activity"]["Video Browsing History"]
-
-    if feed_dict == {}:
-      print("this file has NO video list")
-    else:
-      feed = feed_dict["VideoList"]
-      for i in range(len(feed)):
-        all_links.append(feed[i])
   
+  file_name = dir_path + "/" + file
+  with open(file_name, 'r') as myFile:
+    data = json.load(myFile)
+  myFile.close()
+
+  feed_dict = data["Activity"]["Video Browsing History"]
+  if feed_dict == {}:
+    print("this file has NO video list")
+    return []
+  else:
+    feed = feed_dict["VideoList"]
+    for i in range(len(feed)):
+      all_links.append(feed[i])
   return all_links
 
-data = get_all_links()
+def create_user_json(files):
+  for file in files:
+    data = get_all_links(file)
+    initial = file[10:12]
+    with open(f"1-compile-raw-data/user_data_{initial}.json", "w") as file_to_write:
+      json.dump(data, file_to_write)
+    file_to_write.close()
 
-# write video links to separate json file
-with open("1-compile-raw-data/compiled_data.json", "w") as file_to_write:
-    json.dump(data, file_to_write)
+dir_path = '/users/carolinejung/cs315-private/project2-files' # hardcoded for security purposes
+files = os.listdir(dir_path)
+create_user_json(files)
 
