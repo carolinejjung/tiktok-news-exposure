287f0903ed776052c597740a70da9e28cd7ebcf6
carolinejjung <cjung8321@gmail.com>
2024-03-13 10:38:47 -0400

    downloading video script

--- 4-get-transcriptions/link_to_mp4.py
+++ 4-get-transcriptions/link_to_mp4.py
@@ -1,27 +1,8 @@
 import pyktok as pyk
-import json
+import pandas as pd
 
+data = pd.read_csv("3-filter-metadata/new_relevant_videos.csv")
+data["link"] = [f"https://www.tiktokv.com/share/video/{id}/" for id in data["video_id"]]
 
-filepath = "3-filter-metadata/new_relevant_videos.csv"
-with open(filepath, "r") as filetoread:
-    list_of_dict = json.load(filetoread)
-filetoread.close()
-
-links = [video_dict["link"] for video_dict in list_of_dict]
-print(links)
-# note: last video is "unavailable"
-
-# same issue with KeyError: 'itemInfo'
-# developers: https://github.com/dfreelon/pyktok/blob/main/src/pyktok/pyktok.py
-
-
-# using developer's functions
 pyk.specify_browser('chrome') 
-pyk.save_tiktok_multi_urls(links,False,'mp4_files/tiktok_data.csv',1)
-
-# using for loop
-# for link in links:
-#     pyk.specify_browser('chrome') 
-#     pyk.save_tiktok(link, True, 'video_data.csv','chrome')
-
-# another potential issue: all mp4 files get saved to the main directory and we cannot put it in the mp4_files folder
+pyk.save_tiktok_multi_urls(data["link"],False,'mp4_files/tiktok_data.csv',1)
