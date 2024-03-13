609f1c568af8253a24083f48b6aa0a12e63527ab
carolinejjung <cjung8321@gmail.com>
2024-03-13 11:05:18 -0400

    connect to env

--- 4-get-transcriptions/get_transcriptions.py
+++ 4-get-transcriptions/get_transcriptions.py
@@ -17,9 +17,13 @@ from openai import OpenAI
 # WHISPER_API_KEY = os.environ.get("WHISPER_API_KEY")
 
 import os
-API_KEY = os.getenv("WHISPER_API_KEY")  
-client = OpenAI(api_key=API_KEY)
+# API_KEY = os.getenv("WHISPER_API_KEY")  
+# client = OpenAI(api_key=API_KEY)
 
+# client = OpenAI()
+# OpenAI.api_key = os.getenv('WHISPER_API_KEY')
+
+client = OpenAI()
 
 def transcribe_vid(file):
     audio_file = open(file, "rb")
@@ -27,5 +31,20 @@ def transcribe_vid(file):
     return transcription.text
 
 
-testfile = "4-get-transcriptions/mp4-files/share_video_7062567867182714158_.mp4"
-print(transcribe_vid(testfile))
+# testfile = "4-get-transcriptions/mp4-files/share_video_7062567867182714158_.mp4"
+# print(transcribe_vid(testfile))
+
+# 50 requests per minute limit - sleep 1 second after each request
+# don't run yet -- just do two columns
+
+
+def get_all_transcriptions():
+    files = os.listdir("4-get-transcriptions/mp4-files/")[:2]
+    print(files)
+    
+    # for file in files:
+    #     transcribe_vid(file)
+
+print(get_all_transcriptions())
+#testfile = "4-get-transcriptions/mp4-files/share_video_7062567867182714158_.mp4"
+#print(transcribe_vid(testfile))
