cd7531e43c7333b5f3401ea5cd955e88b7855baa
mluheda <120967274+mluheda@users.noreply.github.com>
2024-03-14 23:55:10 -0400

    filter

--- 4-access-NYT/filterWords.py
+++ 4-access-NYT/filterWords.py
@@ -1,5 +1,6 @@
 import requests, json
 import pandas as pd
+import numpy as np
 from collections import Counter
 import os
 import re
@@ -30,15 +31,68 @@ def filterFuncWords(word_list, csv_file_path): #removes all func words
     return filtered_list
 
 def get_transcriptions():
-    pd.read_csv()
+    data = pd.read_csv("4-get-transcriptions/filtered_w_transcription.csv")
+    return data["transcription"]
 
-def overlapList(list1, ):
+def get_all_articles():
+    #files = os.listdir("4-access-NYT/")
+    #return files
+    all_articles = []
+    with open("4-access-NYT/NYT_2024-1.json") as file1:
+        f1 = json.load(file1)
+    with open("4-access-NYT/NYT_2024-3.json") as file2:
+        f2 = json.load(file2)
+    
+    all_articles.append(f1)
+    all_articles.append(f2)
+    return all_articles
 
-    # all_headlines = all article headlines
+def overlapList():
 
-    article_words = getUniqueWords(all_headlines)
-    article_filtered = filterFuncWords(article_words) #list 1
+    # NYT ARTICLES
+    
+    article_words = getUniqueWords(get_all_articles()[0])
+    article_filtered = filterFuncWords(article_words) 
+    return article_filtered
 
     # embed article_filtered & cluster
 
-    transcriptions
+    # TRANSCRIPTIONS
+    # per video
+    article_vector = text2vector(article_filtered,)
+
+    for vid_transcript in get_transcriptions():
+        # vector of 
+        voc = getVocabulary(vid_transcript)
+        vectorize = text2vector(vid_transcript, voc)
+        
+        #cosine_sim(vid_transcript, article vector)
+
+    return cosine_sim
+
+print(overlapList())
+
+# FOR TRANSCRIPT -----------
+def text2vector(sentence, voc):
+    """Given a sentence and the vocabulary for the problem,
+    turn every sentence into a vector.
+    """
+    cleantext = "".join(char for char in sentence if char not in string.punctuation)
+    words = cleantext.lower().split()
+    vector = [words.count(w) for w in voc]
+    return vector
+
+def getVocabulary(textchunk):
+    """Given some text, create the vocabulary of unique words."""
+    textchunk = textchunk.lower()
+    cleantext = "".join(char for char in textchunk if char not in string.punctuation)
+    words = set(cleantext.split())
+    voc = sorted(words)
+
+    return voc
+# ----------------------------
+
+def cosine_sim(vec1, vec2):
+    v1 = np.array(vec1)
+    v2 = np.array(vec2)
+    return np.dot(v1, v2)/(norm(v1)*norm(v2))
