30a2c5f8c02fc0d18b3e38b308005c6c3f82ccef
carolinejjung <cjung8321@gmail.com>
2024-03-28 14:05:48 -0400

    fix file size issue

--- 4-get-transcriptions/get_transcriptions.py
+++ 4-get-transcriptions/get_transcriptions.py
@@ -35,9 +35,9 @@ def get_transcription_for_file(file, user): # for one file
     mp4_path = f"4-get-transcriptions/mp4-files-{user}/"
 
     try:
-        transcription = transcribe_vid(mp4_path + file)
+        transcription = transcribe_vid(mp4_path + file) # transcribe video
         time.sleep(1)
-        print("SUCCESS: Video was transcribed.") # already done transcribing
+        print("SUCCESS: Video was transcribed.")
         return transcription
     
     except APIStatusError:
@@ -45,19 +45,28 @@ def get_transcription_for_file(file, user): # for one file
         return ""
     
 def data_to_write(user): # for one user
+    """This method only transcribes videos that were actually downloaded as mp4 files. These videos are already
+    filtered and considered as news-relevant videos."""
+
     mp4_path = f"4-get-transcriptions/mp4-files-{user}/"
     files = os.listdir(mp4_path)
 
-    data = pd.read_csv(f"3-filter-metadata/news_relevant_videos_{user}.csv")
-
-    transcripts_all = []
+    file_data = []
     for file in files: 
-        transcripts_all.append(get_transcription_for_file(file, user))
+        # note: this for loop does not go through each video in order of id number
+        filename = file[12:31] # string
+        transcription = get_transcription_for_file(file, user) # string
+        file_data.append([filename, transcription])
+
+    summary = pd.DataFrame(file_data, columns=["VideoID", "Transcription"])
+    return summary
+
+    # #append all the batches of transcriptions together into one flattened list with strings
+    # flattened_list_transcript = [transcript for transcript in transcripts_all]
+    # print("Transcript:", flattened_list_transcript)
+    # data["transcription"] = flattened_list_transcript
+    # return data
     
-    #append all the batches of transcriptions together into one flattened list with strings
-    flattened_list_transcript = [transcript for transcript in transcripts_all]
-    data["transcription"] = flattened_list_transcript
-    return data
 
 # TESTING
 # print(get_all_transcriptions(create_batches("48271")[0], "48271")) # batch size 2
@@ -66,8 +75,14 @@ def data_to_write(user): # for one user
 # TO RUN
 path_to_write = '4-get-transcriptions/final-data-transcribed/'
 # data_to_write("26301").to_csv(path_to_write + 'final_26301.csv') # TO DO
-# data_to_write("33534").to_csv(path_to_write + 'final_33534.csv') # TO DO
+
+
+# data_to_write("33534").to_csv(path_to_write + 'final_33534.csv') # TO DO - error: length of values(64) doesn't match length of index (70)
 # data_to_write("38129").to_csv(path_to_write + 'final_38129.csv') # DONE
-data_to_write("48271").to_csv(path_to_write + 'final_48271.csv') # DONE
+# data_to_write("48271").to_csv(path_to_write + 'final_48271.csv') # DONE
 # data_to_write("69117").to_csv(path_to_write + 'final_69117.csv') # TO DO
 # data_to_write("83721").to_csv(path_to_write + 'final_83721.csv') # RUN LAST (more videos)
+
+# TEST
+#print(data_to_write("48271"))
+#data_to_write("48271").to_csv(path_to_write + 'test_48271.csv') # DONE
