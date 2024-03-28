b4ff8391bab073915409024b9fe38c055f8d0000
carolinejjung <cjung8321@gmail.com>
2024-03-28 16:15:36 -0400

    sample videos

--- 4-get-transcriptions/get_transcriptions.py
+++ 4-get-transcriptions/get_transcriptions.py
@@ -5,7 +5,7 @@ from openai import OpenAI
 from openai import APIStatusError
 import pandas as pd
 import time
-import numpy as np
+import random
 
 # pip install python-decouple
 # from decouple import config
@@ -33,10 +33,9 @@ def transcribe_vid(file):
 
 def get_transcription_for_file(file, user): # for one file
     mp4_path = f"4-get-transcriptions/mp4-files-{user}/"
-
     try:
         transcription = transcribe_vid(mp4_path + file) # transcribe video
-        time.sleep(1)
+        time.sleep(20) # EDIT: 3 requests per minute?? used to be 1
         print("SUCCESS: Video was transcribed.")
         return transcription
     
@@ -50,39 +49,46 @@ def data_to_write(user): # for one user
 
     mp4_path = f"4-get-transcriptions/mp4-files-{user}/"
     files = os.listdir(mp4_path)
+    random.seed(37)
+    sample_files = random.choices(files, k = 25) # randomly sample 25 videos for user
 
     file_data = []
-    for file in files: 
+    for file in sample_files: 
         # note: this for loop does not go through each video in order of id number
         filename = file[12:31] # string
+        print(f"Transcribing video {filename}......")
         transcription = get_transcription_for_file(file, user) # string
         file_data.append([filename, transcription])
+        print("Transcription: ", transcription)
+        print("--------------------------------------------------------------------------------------------------")
 
     summary = pd.DataFrame(file_data, columns=["VideoID", "Transcription"])
     return summary
-
-    # #append all the batches of transcriptions together into one flattened list with strings
-    # flattened_list_transcript = [transcript for transcript in transcripts_all]
-    # print("Transcript:", flattened_list_transcript)
-    # data["transcription"] = flattened_list_transcript
-    # return data
     
-
 # TESTING
 # print(get_all_transcriptions(create_batches("48271")[0], "48271")) # batch size 2
 # print(get_all_transcriptions(create_batches("69117")[1], "69117"))
 
-# TO RUN
+# TO RUN (for all)
 path_to_write = '4-get-transcriptions/final-data-transcribed/'
-# data_to_write("26301").to_csv(path_to_write + 'final_26301.csv') # TO DO
+# data_to_write("26301").to_csv(path_to_write + 'all_26301.csv') # TO DO
+# data_to_write("33534").to_csv(path_to_write + 'all_33534.csv') # DONE
+# data_to_write("38129").to_csv(path_to_write + 'all_38129.csv') # DONE - empty
+# data_to_write("48271").to_csv(path_to_write + 'all_48271.csv') # DONE
+# data_to_write("69117").to_csv(path_to_write + 'all_69117.csv') # TO DO
+# data_to_write("83721").to_csv(path_to_write + 'all_83721.csv') # RUN LAST (more videos)
 
-
-data_to_write("33534").to_csv(path_to_write + 'final_33534.csv') # TO DO - initial error: length of values(64) doesn't match length of index (70)
-# data_to_write("38129").to_csv(path_to_write + 'final_38129.csv') # TO DO - should be empty
-# data_to_write("48271").to_csv(path_to_write + 'final_48271.csv') # DONE
-# data_to_write("69117").to_csv(path_to_write + 'final_69117.csv') # TO DO
-# data_to_write("83721").to_csv(path_to_write + 'final_83721.csv') # RUN LAST (more videos)
+# TO RUN (subset)
+data_to_write("26301").to_csv(path_to_write + 'sample_26301.csv') # TO DO
+# data_to_write("33534").to_csv(path_to_write + 'sample_33534.csv') # TO DO
+# data_to_write("38129").to_csv(path_to_write + 'sample_38129.csv') # DONE but only 1
+# data_to_write("48271").to_csv(path_to_write + 'sample_48271.csv') # TO DO
+# data_to_write("69117").to_csv(path_to_write + 'sample_69117.csv') # TO DO
+# data_to_write("83721").to_csv(path_to_write + 'sample_83721.csv') # TO DO
 
 # TEST
 #print(data_to_write("48271"))
 #data_to_write("48271").to_csv(path_to_write + 'test_48271.csv') # DONE
+
+
+data_to_write("26301").to_csv(path_to_write + 'run2_sample_26301.csv') 
